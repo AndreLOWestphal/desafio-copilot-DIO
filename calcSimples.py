ea02bee3bbb25e6ef971e6efa72be3b100a39136
AndreLOWestphal <dedewestphal@hotmail.com>
2024-03-28 00:58:30 -0300

    Alterei o codigo da calcSimples pois o exercicio estava errado

--- calcSimples.py
+++ calcSimples.py
@@ -2,9 +2,14 @@
 num1 = int(input("Digite um numero inteiro: "))
 num2 = int(input("Digite outro numero inteiro: "))
 
-soma = num1 + num2
-subtracao = num1 - num2
-multiplicacao = num1 * num2
-divisao = num1 / num2
+operacao = input("Digite qual operação deseja fazer (+,-,/,*): ")
 
-print("Soma: ", soma , " Subtração: " , subtracao , " Multiplicação: " , multiplicacao , " Divisão: " , divisao)
+if(operacao == "+"):
+    print("A soma é: ", num1 + num2)
+elif(operacao == "-"):
+    print("A subtração é: ", num1 - num2)
+elif(operacao == "/"):
+    print("A divisão é: ", num1 / num2)
+elif(operacao == "*"):
+    print("A multiplicação é: ", num1 * num2)
+else:print("Error")
